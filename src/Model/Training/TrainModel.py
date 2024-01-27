254e4025bc4e66699728cab5b8a775dbd4b38fc3
lorenzo.brucato <lorenzo.brucato@universite-paris-saclay.fr>
2024-01-27 16:56:12 +0100

    structure + data generation

---
+++ src/Model/Training/TrainModel.py
@@ -0,0 +1,21 @@
+import torch 
+import torch.nn as nn
+import torch.optim as optim 
+import torch.nn.functional as F
+
+class TrainModel():
+
+    def __init__(self):
+        #TODO
+        pass
+
+    def train(self):
+        #TODO
+        pass
+
+    def test(self):
+        #TODO
+        pass
+
+
+    
