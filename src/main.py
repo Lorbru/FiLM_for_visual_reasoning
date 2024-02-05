29407c9697e7b86333eeaff86238f563458c9059
Alexandre <alexou.loret@gmail.com>
2024-02-05 11:21:14 +0100

    first cnn test

--- src/main.py
+++ src/main.py
@@ -10,22 +10,22 @@ from Model.Architecture.CNN import CNN
 from torchvision import transforms
 
 
-def first_CNN(archi=CNN, img_size=180, input_shape=3, output_shape=16, device="cpu", n_epochs=10, lr=0.01):
+def first_CNN(archi=CNN, img_size=180, input_shape=3, output_shape=16, device="cpu", n_images=500, n_epochs=10, lr=0.01):
 
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((.5, .5, .5), (.5, .5, .5))
     ])
 
-    batch_size = 5
+    batch_size = 32
     with open('src/DataGenerator/json/LabelsMaps.json', 'r') as f:
-        labels = json.load(f)
+        labelsMaps = json.load(f)
 
-    labelsInv = dict(zip(labels.values(), labels.keys()))
+    labelsInv = dict(zip(labelsMaps.values(), labelsMaps.keys()))
 
     TrainLoader = []
     question = QAFactory.randomQuestion(qtype="position", dirAlea="au centre")
-    for i in range(20):
+    for i in range(n_images):
         DataGen = DataGenerator()
         _, answer, img = DataGen.buildImageFromQA(question)
         print(answer,int(labelsInv[answer]))
@@ -34,6 +34,9 @@ def first_CNN(archi=CNN, img_size=180, input_shape=3, output_shape=16, device="c
         img = transform(img.img)
         img = img.unsqueeze(0)
         TrainLoader += [(img, answer)]
+
+    print("====== IMAGES GENEREES ======")
+
 
     model = archi(img_size, input_shape, output_shape)
 
@@ -59,15 +62,37 @@ def first_CNN(archi=CNN, img_size=180, input_shape=3, output_shape=16, device="c
         running_loss = 0.0
     return model
 
-first_CNN()
-
 def main():
 
     print("====== RUNNING PROJECT ======")
 
-    
+    print(torch.cuda.is_available())
+    mod = first_CNN(n_epochs=5, n_images=2000, output_shape=4)
 
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((.5, .5, .5), (.5, .5, .5))
+    ])
 
+    batch_size = 256
+    with open('src/DataGenerator/json/LabelsMaps.json', 'r') as f:
+        labelsMaps = json.load(f)
+
+    labelsInv = dict(zip(labelsMaps.values(), labelsMaps.keys()))
+
+    question = QAFactory.randomQuestion(qtype="position", dirAlea="au centre")
+    for i in range(10):
+        DataGen = DataGenerator()
+        _, answer, img = DataGen.buildImageFromQA(question)
+        print(answer, int(labelsInv[answer]))
+        # answer = torch.tensor([int(labelsInv[answer])])
+        # img.saveToPNG("src/Data/DataGen/T0.png")
+        img = transform(img.img)
+        img = img.unsqueeze(0)
+
+        output = mod(img)
+        # print(output)
+        print(output.argmax())
 
 
 
