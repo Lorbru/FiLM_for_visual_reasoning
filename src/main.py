c9351650fa3e8add986106529e1ec96a518e14fc
alexo <alexou.loret@gmail.com>
2024-02-10 16:19:40 +0100

    le cnn marche enfaite

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from Model.Architecture.CNN import CNN
 from torchvision import transforms
 
 
-def first_CNN(archi=CNN, img_size=180, input_shape=3, output_shape=16, device="cpu", n_images=500, n_epochs=10, lr=0.01, batch_size = 256, unique=False):
+def first_CNN(archi=CNN, img_size=180, input_shape=3, output_shape=16, device="cpu", n_images=500, n_epochs=10, lr=0.01, batch_size = 256, unique=False, model=None):
 
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -43,7 +43,8 @@ def first_CNN(archi=CNN, img_size=180, input_shape=3, output_shape=16, device="c
 
     print("-> Images Générées")
 
-    model = archi(img_size, input_shape, output_shape).to(device)
+    if model == None:
+        model = archi(img_size, input_shape, output_shape).to(device)
 
     optimizer = torch.optim.NAdam(model.parameters(), lr=lr)
     criterion = torch.nn.CrossEntropyLoss()
@@ -58,13 +59,14 @@ def first_CNN(archi=CNN, img_size=180, input_shape=3, output_shape=16, device="c
 
             # forward + backward + optimize
             outputs = model(inputs)
+
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
 
         print(f'Epoch {epoch} / {n_epochs} | Loss: {running_loss / len(TrainLoader)}')
-        if (epoch-1)%10 == 0 :
+        if (epoch+1)%10 == 0 and epoch != 1 :
             torch.save(model.state_dict(), "src/Data/mod"+str(epoch)+".pth")
         running_loss = 0.0
     return model
@@ -82,11 +84,13 @@ def main():
 
     print("====== RUNNING PROJECT ======")
 
-    unique = True
-    mod = first_CNN(n_epochs=50, n_images=1000, output_shape=4, device=device, unique=unique, lr=0.00001)
+    unique = False
+    mod = first_CNN(n_epochs=50, n_images=50000, output_shape=4, device=device, unique=unique, lr=0.00001)
+
     # mod = CNN(180, 3, 4).to(device)
     # mod.load_state_dict(torch.load("src/Data/mod31.pth"))
     # mod.eval()
+    # mod = first_CNN(n_epochs=50, n_images=5000, output_shape=4, device=device, unique=unique, lr=0.00001, model=mod)
 
     print("====== RUNNING TESTS ======")
 
@@ -101,25 +105,26 @@ def main():
     labelsInv = dict(zip(labelsMaps.values(), labelsMaps.keys()))
 
     DataGen = DataGenerator()
+    n_tests = 1000
     if unique :
-        for i in range(10):
+        sum = 0
+        for i in range(n_tests):
             answer, img = DataGen.buildUniqueImageFromFigure()
-            print(answer, int(labelsInv[answer]))
             img = transform(img.img)
             img = img.unsqueeze(0).to(device)
             output = mod(img)
-            print(output.argmax())
+            sum += int(labelsInv[answer]) == int(output.argmax())
+        print("Accuracy : "+str(sum/n_tests*100)+"%")
     else :
+        sum = 0
         question = QAFactory.randomQuestion(qtype="position", dirAlea="au centre")
-        for i in range(10):
+        for i in range(n_tests):
             _, answer, img = DataGen.buildImageFromQA(question)
-            print(answer, int(labelsInv[answer]))
             img = transform(img.img)
             img = img.unsqueeze(0).to(device)
             output = mod(img)
-            print(output.argmax())
-
-    torch.save(mod.state_dict(), "src/Data/mod_final.pth")
+            sum += int(labelsInv[answer]) == int(output.argmax())
+        print("Accuracy : " + str(sum / n_tests * 100) + "%")
 
     print("======       END      =======")
     return 
